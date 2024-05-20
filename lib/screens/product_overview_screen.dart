fef82deae635ec1c6853dabd12c992056748fba1
Hubertkin <kingsleyocran94@gmail.com>
2024-05-20 23:26:40 +0000

    added a popup menu button to the appbar to show favorites and all items

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -2,20 +2,39 @@ import 'package:flutter/material.dart';
 import '../widgets/product_grid.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
- 
-    
-
   const ProductOverviewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.7),
+          backgroundColor:
+              Theme.of(context).colorScheme.primary.withOpacity(0.7),
           title: const Text('My Shop'),
+          actions: [
+            PopupMenuButton(
+              icon: const Icon(Icons.more_vert),
+              onSelected: (int selectedValue) {
+                if (selectedValue == 0) {
+                  // show only favorites
+                } else {
+                  // show all
+                }
+                debugPrint(selectedValue.toString());
+              },
+              itemBuilder: (_) => const [
+                PopupMenuItem(
+                  value: 0,
+                  child: Text('Only Favorites'),
+                ),
+                PopupMenuItem(
+                  value: 1,
+                  child: Text('Show All'),
+                )
+              ],
+            )
+          ],
         ),
         body: const ProductGrid());
   }
 }
-
-
