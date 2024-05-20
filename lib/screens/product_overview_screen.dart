2ca2dadad1d514acf41f4cbb63ea525455bde919
Hubertkin <kingsleyocran94@gmail.com>
2024-05-20 23:40:12 +0000

    implementation of favorite function

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/product_provider.dart';
 import '../widgets/product_grid.dart';
 
+enum FilterOptions
+{
+  favorites,
+  all
+}
 class ProductOverviewScreen extends StatelessWidget {
   const ProductOverviewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final productContainer = Provider.of<ProductProvider>(context);
     return Scaffold(
         appBar: AppBar(
           backgroundColor:
@@ -14,21 +22,21 @@ class ProductOverviewScreen extends StatelessWidget {
           actions: [
             PopupMenuButton(
               icon: const Icon(Icons.more_vert),
-              onSelected: (int selectedValue) {
-                if (selectedValue == 0) {
-                  // show only favorites
+              onSelected: (FilterOptions selectedValue) {
+                if (selectedValue == FilterOptions.favorites) {
+                  productContainer.showFavoritesOnly();
                 } else {
-                  // show all
+                  productContainer.showAll();
                 }
                 debugPrint(selectedValue.toString());
               },
               itemBuilder: (_) => const [
                 PopupMenuItem(
-                  value: 0,
+                  value: FilterOptions.favorites,
                   child: Text('Only Favorites'),
                 ),
                 PopupMenuItem(
-                  value: 1,
+                  value: FilterOptions.all,
                   child: Text('Show All'),
                 )
               ],
