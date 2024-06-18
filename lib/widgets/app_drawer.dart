7a0fca26b5259c16df2a98bfdf94296b3babb57e
Hubertkin <kingsleyocran94@gmail.com>
2024-06-18 23:27:49 +0000

    slight changes

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -33,7 +33,7 @@ class AppDrawer extends StatelessWidget {
           const Divider(),
           ListTile(
             leading: const Icon(Icons.edit),
-            title: const Text('Manage Product'),
+            title: const Text('Manage Products'),
             onTap: () {
               Navigator.of(context).pushReplacementNamed(UserProductScreen.routeName);
             },
