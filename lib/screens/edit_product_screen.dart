ea31a86f40ac36c1c311d3f3e2ab805181b95a06
Hubertkin <kingsleyocran94@gmail.com>
2024-07-02 23:54:17 +0000

    Add focusnode to Form widget

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -9,6 +9,7 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,12 +24,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ),
             textInputAction: TextInputAction.next,
           ),
-          TextFormField( 
+          TextFormField(
+            onFieldSubmitted: (_) =>
+                FocusScope.of(context).requestFocus(_priceFocusNode),
             decoration: const InputDecoration(
               labelText: 'Price',
             ),
             textInputAction: TextInputAction.next,
             keyboardType: TextInputType.number,
+            focusNode: _priceFocusNode,
           )
         ]),
       ),
